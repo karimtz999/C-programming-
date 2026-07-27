e4c70f885bc93b3777ed5cfc6bcbd98c2372b047
karimtz999 <farkarim36@gmail.com>
2026-07-28 00:02:29 +0100

    Auto-build and push update

---
+++ 1464. Maximum Product of Two Elements in an Array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int maxProduct(int* nums, int numsSize) {
+    int first_max = 0, second_max = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] > first_max) {
+            second_max = first_max;
+            first_max = nums[i];
+        } else if (nums[i] > second_max) {
+            second_max = nums[i];
+        }
+    }
+
+    return (first_max - 1) * (second_max - 1);
+}
+
+// Example usage
+int main() {
+    int nums[] = {2, 1, 5, 6, 2, 3};
+    int size = sizeof(nums) / sizeof(nums[0]);
+
+    printf("Maximum Product: %d\n", maxProduct(nums, size));
+    return 0;
+}
---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Compile code.c into code.exe, run it, then push changes
+
+code.exe
+

86110c0c8a3432f50bbe5cb62c8cb1d7bbc2d2b8
karimtz999 <farkarim36@gmail.com>
2026-07-28 11:11:52 +0100

    Auto-build and push update

---
+++ C_notes/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h> //It allows the use of input and output functions, such as printf()
+
+int main (){
+    int age = 25;
+
+    printf("I am %d years old.\n", age);
+
+
+
+
+
+    return 0;
+}

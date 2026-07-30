37db0f0c4e9f9d074f10b440f81deff691e042a1
karimtz999 <farkarim36@gmail.com>
2026-07-30 15:51:57 +0100

    Auto-build and push update

--- C_notes/cat.c
+++ C_notes/cat.c
@@ -3,6 +3,10 @@
 int main() {
     int i;
     for (i = 0; i < 10; i++) {
+        if (i == 2) {
+            printf("Skipping iteration when i is 2\n");
+            continue;
+        }
         printf("Hello, World!\n");
     }
     return 0;

7c16095c8ec13b1e1c22256fc90eeb9ff1dd79e8
karimtz999 <farkarim36@gmail.com>
2026-08-02 20:27:32 +0100

    Auto-build and push update

--- predict_the_winner.c
+++ predict_the_winner.c
@@ -4,13 +4,21 @@
 bool predictTheWinner(int* nums, int numsSize)
 {
     printf("%d\n", numsSize);
-    printf("%d\n", nums[2]);
-    return true;
+    return 0;
+}
+
+bool catchhim(int* nums, int numsSize)
+{
+    int list = 0;
+    nums[1] = 10 ;
+    return 0;
 }
 
 int main(void)
 {
-    int nums[] = {1, 5, 2};
-    predictTheWinner(nums, 3);
+    int nums[] = {1, 5, 2, 4};
+    predictTheWinner(nums, 5);
+    catchhim(nums, 3);
+    printf("{%d %d %d}\n",nums[0], nums[1], nums[2]);
     return 0;
 }

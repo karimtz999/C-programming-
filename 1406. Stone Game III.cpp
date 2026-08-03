fa97f7df0b61ec0ce2ba75e370fb13c3b3ad2301
karimtz999 <farkarim36@gmail.com>
2026-08-03 23:49:06 +0100

    Auto-build and push update

--- 1406. Stone Game III.cpp
+++ 1406. Stone Game III.cpp
@@ -5,7 +5,7 @@ public:
     string stoneGameIII(vector<int>& stoneValue)
     {
         n = stoneValue.size();
-        dp.assign(n, <int>(2, INT_MIN));
+        dp.assign(n, vector<int>(2, INT_MIN));
 
         int result = dfs(0, 1, stoneValue);
         if (result == 0) return "Tie";

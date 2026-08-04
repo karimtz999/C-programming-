fa3e833ff07dd284a9d8ec97753845073ab12398
karimtz999 <farkarim36@gmail.com>
2026-08-04 21:06:31 +0100

    Auto-build and push update

--- 1. Two Sum/Two_Sum.c
+++ 1. Two Sum/Two_Sum.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void Two_Sum (int nums[], int target, int size)
+void Two_Sum (int nums[], int target, int size) //size = 4 , nums = {2,7,11,15} , target = 26
 {
     for (int i = 0; i < size; i++)
     {
@@ -16,9 +16,13 @@ void Two_Sum (int nums[], int target, int size)
 }
 int main()
 {
-    int nums[] = {3,3};
-    int target = 6;
-    int size = sizeof(nums) / sizeof(nums[0]);
+    int nums[] = {2,7,11,15};
+    int target = 26;
+    // 4 elements × 4 bytes = 16 bytes
+    // nums[0] is just the first integer which mean 2
+    // sizeof(nums)   // 16
+    // sizeof(nums[0])   // 4
+    int size = sizeof(nums) / sizeof(nums[0]); // 16 / 4 = 4
     Two_Sum(nums, target, size);
     return 0;
 }

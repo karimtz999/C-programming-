b17fb8f5985f98f9c67519a1db81478f7cb355a5
karimtz999 <farkarim36@gmail.com>
2026-08-04 21:27:05 +0100

    Auto-build and push update

--- 1. Two Sum/Two_Sum.c
+++ 1. Two Sum/Two_Sum.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
 
-void Two_Sum (int nums[], int target, int size) //size = 4 , nums = {2,7,11,15} , target = 26
-{
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = i + 1; j < size; j++)
-        {
-            if (nums[i] + nums[j] == target)
-            {
-                printf("[%d,%d]\n",i,j);
-                return;
-            }
-        }
-    }
-}
 int main()
 {
-    int nums[] = {2,7,11,15};
+    int nums[] = {2, 7, 11, 15};
     int target = 26;
-    // 4 elements × 4 bytes = 16 bytes
-    // nums[0] is just the first integer which mean 2
-    // sizeof(nums)   // 16
-    // sizeof(nums[0])   // 4
-    int size = sizeof(nums) / sizeof(nums[0]); // 16 / 4 = 4
+
+    int size = sizeof(nums) / sizeof(nums[0]);
+
     Two_Sum(nums, target, size);
+
+    freeTable();
+
     return 0;
 }

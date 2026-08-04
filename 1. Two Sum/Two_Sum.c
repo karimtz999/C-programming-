4c32bfa6c0076a8b57f5cc5a1380f3a9d7837eb8
karimtz999 <farkarim36@gmail.com>
2026-08-04 17:20:32 +0100

    Auto-build and push update

--- 1. Two Sum/Two_Sum.c
+++ 1. Two Sum/Two_Sum.c
@@ -1,27 +1,25 @@
 
 #include <stdio.h>
 
-int main() {
-    void Two_Sum (int nums[], int target, int size)
+void Two_Sum (int nums[], int target, int size)
+{
+    for (int i = 0; i < size; i++)
     {
-        for (int i = 0; i < size; i++)
+        for (int j = i + 1; j < size; j++)
         {
-            for (int j = i + 1; j < size; j++)
+            if (nums[i] + nums[j] == target)
             {
-                if (nums[i] + nums[j] == target)
-                {
-                    printf("[%d,%d]\n",i,j);
-                    return;
-                }
+                printf("[%d,%d]\n",i,j);
+                return;
             }
         }
     }
-    int main()
-    {
-        int nums[] = {2,7,11,15};
-        int target = 9;
-        int size = sizeof(nums) / sizeof(nums[0]);
-        Two_Sum(nums, target, size);
-        return 0;
-    }
+}
+int main()
+{
+    int nums[] = {2,7,11,15};
+    int target = 9;
+    int size = sizeof(nums) / sizeof(nums[0]);
+    Two_Sum(nums, target, size);
+    return 0;
 }
